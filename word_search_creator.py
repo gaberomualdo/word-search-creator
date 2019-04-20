8bd9ff37749dbb893a3ea4f53f8d2a75a17107d4
xtrp <36894883+xtrp@users.noreply.github.com>
2019-04-20 21:00:37 +0100

    refined test case and made grid-size generation more effecient and effective

--- word_search_creator.py
+++ word_search_creator.py
@@ -1,17 +1,20 @@
 def createWordSearch(words):
-	gridSideLength = 0
-	for word in words:
-		# grid side length is 4 added to the length of the longest word
-		# --> makes word search harder and possible to generate with given words and grid size
-		if(gridSideLength - 4 < len(word)):
-			gridSideLength = len(word) + 4
+	# grid side length is the length of the longest word
+	# --> makes word search possible to generate with given words and grid size
+	gridSideLength = max(words, key=len)
 
 	# generate 2d array with side lengths of gridSideLength, with each item having a content of ""
-	grid = [[""]*gridSideLength]*gridSideLength
+	grid = [["*"]*gridSideLength]*gridSideLength
+
+	return grid
 
 	# loop through words and place them randomly in the grid, either horizontally, vertically, or diagonally
 	for word in words:
 		# === CODE HERE ===
+		pass
+	pass
 
 # test createWordSearch function with 8 randomly generated words
-print createWordSearch(["seemly", "exotic", "obese", "disagreeable", "earn", "spark", "strengthen", "colossal"])
+returnedWordSearch = createWordSearch(["seemly", "exotic", "obese", "disagreeable", "earn", "spark", "strengthen", "colossal"])
+for row in returnedWordSearch:
+	print ' '.join(row)
