d28871c30330b7dba8086da42dd684e01c153a6c
xtrp <36894883+xtrp@users.noreply.github.com>
2019-04-21 04:35:50 +0100

    wrote algorithm and finished word search function

--- word_search_creator.py
+++ word_search_creator.py
@@ -4,30 +4,69 @@ import random
 def createWordSearch(words):
 	# grid side length is the length of the longest word
 	# --> makes word search possible to generate with given words and grid size
-	gridSideLength = len(max(words, key=len))
+	gridSideLength = len(max(words, key=len)) + 6
 
 	# generate 2d array with side lengths of gridSideLength, with each item having a content of ""
-	grid = [[""]*gridSideLength]*gridSideLength
+	grid = []
+	for rowIndex in range(gridSideLength):
+		row = []
+		for columnIndex in range(gridSideLength):
+			row.append("")
+		grid.append(row)
 
 	# loop through words and place them randomly in the grid, either horizontally, vertically, or diagonally
 	for word in words:
 		# choose randomly between possible placement types (horizontal, vertical, and diagonal)
 		placementType = random.choice(["horizontal", "vertical", "diagonal"])
 
+		# placement location variable ( [x, y] )
+		placementLocation = [0, 0]
+
 		# placement status (when placed, placement status is True and while loop ends)
 		placementStatus = False
+
 		while(placementStatus == False):
 			# create grid copy to be edited in testing
-			gridCopy = deepcopy(grid)
+			#gridCopy = deepcopy(grid)
 
 			# create random coordinates for placement location
 			placementLocation = [random.randrange(gridSideLength), random.randrange(gridSideLength)]
 			if(placementType == "horizontal" or placementType == "diagonal"):
-				placementLocation[0] = random.randrange(gridSideLength - len(word))
+				if(gridSideLength - len(word) == 0):
+					placementLocation[0] = 0
+				else:
+					placementLocation[0] = random.randrange(gridSideLength - len(word))
 			if(placementType == "vertical" or placementType == "diagonal"):
-				placementLocation[1] = random.randrange(gridSideLength - len(word))
+				if(gridSideLength - len(word) == 0):
+					placementLocation[1] = 0
+				else:
+					placementLocation[1] = random.randrange(gridSideLength - len(word))
+			
+			# set placement status to True --> to be changed back to False if there is overlap in placement location
+			placementStatus = True
 
-			print "Type: " + placementType + "\nPosition: " + str(placementLocation) + "\n======"
+			# loop through word and letter placement locations to check if placement location doesn't overlap with other words
+			for letterIndex in range(len(word)):
+				letter = word[letterIndex]
+				letterLocation = [placementLocation[0], placementLocation[1]]
+				if(placementType == "horizontal" or placementType == "diagonal"):
+					letterLocation[0] += letterIndex
+				if(placementType == "vertical" or placementType == "diagonal"):
+					letterLocation[1] += letterIndex
+				
+				if(grid[letterLocation[0]][letterLocation[1]] != "" and grid[letterLocation[0]][letterLocation[1]] != letter):
+					placementStatus = False
+
+		for letterIndex in range(len(word)):
+			letter = word[letterIndex]
+			letterLocation = [placementLocation[0], placementLocation[1]]
+			if(placementType == "horizontal" or placementType == "diagonal"):
+				letterLocation[0] += letterIndex
+			if(placementType == "vertical" or placementType == "diagonal"):
+				letterLocation[1] += letterIndex
+			
+			grid[letterLocation[0]][letterLocation[1]] = letter
+
 	# fill the rest of grid with random letters
 	for rowIndex in range(len(grid)):
 		row = deepcopy(grid[rowIndex])
@@ -37,10 +76,6 @@ def createWordSearch(words):
 		grid[rowIndex] = deepcopy(row)
 
 	return grid
-
-
-		
-	
 
 # convert word search grid to printable string
 def stringifyWordSearch(wordSearchArray):
