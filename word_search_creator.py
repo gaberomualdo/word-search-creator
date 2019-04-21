2ca10e37c2471c58bd8b3eae2f16b2f2031fac8d
xtrp <36894883+xtrp@users.noreply.github.com>
2019-04-21 03:55:36 +0100

    scrapped current algorithm idea

--- word_search_creator.py
+++ word_search_creator.py
@@ -15,48 +15,14 @@ def createWordSearch(words):
 
 	# loop through words and place them randomly in the grid, either horizontally, vertically, or diagonally
 	for word in words:
-		# make copy of current grid for later use
-		currentGrid = deepcopy(grid)
-		print currentGrid
-
-		# list of all possible grids after word is placed in grid
-		possibleNewGrids = []
-
-		# loop rows and columns
-		for rowIndex in range(len(grid)):
-			row = deepcopy(grid[rowIndex])
-			for itemIndex in range(len(row)):
-				# get possible grids for horizontal, vertical, and diagonal placement
-				for placementType in ["horizontal", "vertical", "diagonal"]:
-					if(placementType == "horizontal"):
-						# loop through and place each letter of word
-						for letterIndex in range(len(word)):
-							letter = word[letterIndex]
-							try:
-								if(row[itemIndex + letterIndex].replace(letter, "") == ""):
-									row[itemIndex + letterIndex] = letter
-								else:
-									yieldError()
-							except:
-								break
-						if(letterIndex == len(word) - 1):
-							grid[rowIndex] = deepcopy(row)
-							possibleNewGrids.append(deepcopy(grid))
-						grid = deepcopy(currentGrid)
-
-					elif(placementType == "vertical"):
-						pass
-					elif(placementType == "diagonal"):
-						pass
-
-		grid = deepcopy(random.choice(possibleNewGrids))
+		pass
 
 	# fill the rest of grid with random letters
 	for rowIndex in range(len(grid)):
 		row = deepcopy(grid[rowIndex])
 		for itemIndex in range(len(row)):
 			if(row[itemIndex] == ""):
-				row[itemIndex] = random.choice(list("*"))
+				row[itemIndex] = random.choice(list("abcdefghijklmnopqrstuvwxyz"))
 		grid[rowIndex] = deepcopy(row)
 
 	return grid
@@ -75,4 +41,4 @@ def stringifyWordSearch(wordSearchArray):
 
 # test createWordSearch function with 8 randomly generated words
 returnedWordSearch = createWordSearch(["seemly", "exotic", "obese", "disagreeable", "earn", "spark", "strengthen", "colossal"])
-print stringifyWordSearch(returnedWordSearch)
+#print stringifyWordSearch(returnedWordSearch)
