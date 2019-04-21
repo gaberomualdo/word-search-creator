71250283600fa678824f9b5e51c52d3b68836b55
xtrp <36894883+xtrp@users.noreply.github.com>
2019-04-21 02:11:43 +0100

    algorithm updates

--- word_search_creator.py
+++ word_search_creator.py
@@ -1,18 +1,69 @@
+from copy import copy, deepcopy
+import random
+
+# function to yield an error (by requesting second index of empty array) --> used to move to the except section within try-except statements
+def yieldError():
+	return [][1]
+
 def createWordSearch(words):
 	# grid side length is the length of the longest word
 	# --> makes word search possible to generate with given words and grid size
 	gridSideLength = len(max(words, key=len))
 
 	# generate 2d array with side lengths of gridSideLength, with each item having a content of ""
-	grid = [["*"]*gridSideLength]*gridSideLength
-
-	return grid
+	grid = [[""]*gridSideLength]*gridSideLength
 
 	# loop through words and place them randomly in the grid, either horizontally, vertically, or diagonally
 	for word in words:
-		# === CODE HERE ===
-		pass
-	pass
+		# make copy of current grid for later use
+		currentGrid = deepcopy(grid)
+		print currentGrid
+
+		# list of all possible grids after word is placed in grid
+		possibleNewGrids = []
+
+		# loop rows and columns
+		for rowIndex in range(len(grid)):
+			row = deepcopy(grid[rowIndex])
+			for itemIndex in range(len(row)):
+				# get possible grids for horizontal, vertical, and diagonal placement
+				for placementType in ["horizontal", "vertical", "diagonal"]:
+					if(placementType == "horizontal"):
+						# loop through and place each letter of word
+						for letterIndex in range(len(word)):
+							letter = word[letterIndex]
+							try:
+								if(row[itemIndex + letterIndex].replace(letter, "") == ""):
+									row[itemIndex + letterIndex] = letter
+								else:
+									yieldError()
+							except:
+								break
+						if(letterIndex == len(word) - 1):
+							grid[rowIndex] = deepcopy(row)
+							possibleNewGrids.append(deepcopy(grid))
+						grid = deepcopy(currentGrid)
+
+					elif(placementType == "vertical"):
+						pass
+					elif(placementType == "diagonal"):
+						pass
+
+		grid = deepcopy(random.choice(possibleNewGrids))
+
+	# fill the rest of grid with random letters
+	for rowIndex in range(len(grid)):
+		row = deepcopy(grid[rowIndex])
+		for itemIndex in range(len(row)):
+			if(row[itemIndex] == ""):
+				row[itemIndex] = random.choice(list("*"))
+		grid[rowIndex] = deepcopy(row)
+
+	return grid
+
+
+		
+	
 
 # convert word search grid to printable string
 def stringifyWordSearch(wordSearchArray):
